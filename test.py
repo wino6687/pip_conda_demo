c58afb2d4a4e5798f2d93db90a94bb98c6bc6886
wino6687 <wino6687@colorado.edu>
2019-01-09 12:49:37 -0700

    updating to match new repo name

--- test.py
+++ test.py
@@ -2,5 +2,10 @@ from conda_demo import conda_demo
 import pytest
 
 def test_hello():
+    '''pytest works by defining functions that beging with "test"
+
+    You start by performing an action your library should be capabale of doing,
+    then assert that the action yielded the expected result!
+    '''
     response = conda_demo.demo()
     assert response = 'hello world!'
