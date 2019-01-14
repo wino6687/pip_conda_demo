7586228026d0a4ccdefea6d7db5ae8669024f8e3
wino6687 <wino6687@colorado.edu>
2019-01-14 11:14:46 -0700

    trying to add a test case for mock server, probably will throw a travis error for now

--- pip_conda_demo/tests/test_hello.py
+++ pip_conda_demo/tests/test_hello.py
@@ -1,5 +1,7 @@
 from pip_conda_demo import pip_conda_demo
 import pytest
+import requests
+import glob
 
 def test_hello():
     '''pytest works by defining functions that beging with "test"
@@ -8,3 +10,10 @@ def test_hello():
     then assert that the action yielded the expected result!
     '''
     assert pip_conda_demo.demo() == 'hello world!'
+
+def test_scrape():
+    url = 'http://localhost:8000/data/data_file.txt'
+    r = requests.get(url, allow_redirects=True)
+    open('text_file.txt','wb').write(r.content)
+    file_list = glob.glob("*.txt")
+    assert file_list[0] == 'data_file.txt'
