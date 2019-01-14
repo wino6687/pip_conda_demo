b9e83f98d03af8e03be57dcc760429ec51dbb60d
wino6687 <wino6687@colorado.edu>
2019-01-14 12:07:52 -0700

    added doc string to test file to tell users what is going on

--- pip_conda_demo/tests/test_hello.py
+++ pip_conda_demo/tests/test_hello.py
@@ -4,7 +4,8 @@ import requests
 import glob
 
 def test_hello():
-    '''pytest works by defining functions that beging with "test"
+    '''
+    pytest works by defining functions that beging with "test"
 
     You start by performing an action your library should be capabale of doing,
     then assert that the action yielded the expected result!
@@ -12,6 +13,13 @@ def test_hello():
     assert pip_conda_demo.demo() == 'hello world!'
 
 def test_scrape():
+    '''
+    The mock server is started up by travis CI during testing
+
+    Using requests we can scrape the data from our local server!
+    Then we verify that the file was downloaded with glob (there are
+    other ways to go about verifying the download succeeded as well)
+    '''
     url = 'http://localhost:8000/data/data_file.txt'
     r = requests.get(url, allow_redirects=True)
     open('data_file.txt','wb').write(r.content)
